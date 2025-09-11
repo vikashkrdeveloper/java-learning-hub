f4e7147bdd915ea318611b8e4e31fef6ef94ea5b
Vikash Kumar <vikashjjp728@gmail.com>
2025-09-11 09:25:35 +0530

    Add one comment line in the code.

--- codes/Basics/src/Main.java
+++ codes/Basics/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Jai Shri Krishna!");
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            System.out.println("i = " + i); // Print the value of i in each iteration [1, 2, 3, 4, 5]
         }
     }
 }
