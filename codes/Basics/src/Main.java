3fab942a5f211b39a772cd8c7957a5ee9462aa52
Vikash Kumar <vikashjjp728@gmail.com>
2025-09-22 07:29:51 +0530

    Add one comment line in the code.

--- codes/Basics/src/Main.java
+++ codes/Basics/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Jai Shri Krishna!");
+        System.out.println("Jai Shri Krishna!|| Jai Shri Ram!"); // Print a greeting message
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i); // Print the value of i in each iteration [1, 2, 3, 4, 5]
+            System.out.println("i = ==> (" + i + ")"); // Print the value of i in each iteration [1, 2, 3, 4, 5]
         }
     }
 } 
--- codes/Basics/src/StringCodes.java
+++ codes/Basics/src/StringCodes.java
@@ -1,26 +1,26 @@
 public class StringCodes {
     public static void main(String[] args) {
-        String str = "Hello, World!";
+        String str = "Hello, World! ==> Java String Methods";
 
         // Get the length of the string
         int length = str.length();
-        System.out.println("Length: ==> " + length);
+        System.out.println("Length: ==> (" + length + ")");
 
         // Get a character at a specific index
         char charAtIndex = str.charAt(7);
-        System.out.println("Character at index 7: ==> " + charAtIndex);
+        System.out.println("Character at index 7: ==> (" + charAtIndex + ")");
 
         // Get the Unicode code point of a character at a specific index
         int codePointAtIndex = str.codePointAt(7);
-        System.out.println("Unicode code point at index 7: ==> " + codePointAtIndex);
- 
+        System.out.println("Unicode code point at index 7: ==> (" + codePointAtIndex + ")");
+
         // Get the Unicode code point before a specific index
         int codePointBeforeIndex = str.codePointBefore(7);
-        System.out.println("Unicode code point before index 7: ==> " + codePointBeforeIndex);
+        System.out.println("Unicode code point before index 7: ==> (" + codePointBeforeIndex + ")");
 
         // Get the Unicode code point count in a substring
         int codePointCount = str.codePointCount(0, str.length());
-        System.out.println("Total Unicode code points in the string: ==>" + codePointCount);
+        System.out.println("Total Unicode code points in the string: ==> (" + codePointCount + ")");
 
         // Convert the string to an array of characters
         char[] charArray = str.toCharArray();
