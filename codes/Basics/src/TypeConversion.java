734313c12acfe88c1aa4ffc94daed3351d00e235
Vikash Kumar <vikashjjp728@gmail.com>
2025-10-03 22:14:29 +0530

    Solve more problem on 03/10/2025

---
+++ codes/Basics/src/TypeConversion.java
@@ -0,0 +1,22 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        // Type Conversion
+        // Type1: Implicit Type Conversion (Widening) - Automatic (Coercion)
+        int intValue = 44;
+        double doubleValue = intValue; // int to double
+        System.out.println("Implicit Type Conversion (Widening): ==> (" + doubleValue + ")");
+        long big = 45;
+        float dec = 5;
+        double d = 5.4f;
+        System.out.println("Implicit Type Conversion (Widening) examples: ==> (" + big + "), (" + dec + "), (" + d + ")");
+
+        // Type2: Explicit Type Conversion (Narrowing) - Manual (Casting)
+        double anotherDoubleValue = 9.78;
+        int anotherIntValue = (int) anotherDoubleValue; // double to int
+        System.out.println("\nExplicit Type Conversion (Narrowing): ==> (" + anotherIntValue + ")");
+        float eDic = (float) 5.4;
+        long eBig = (long) 5.4;
+        int eInt = (int) 5.4;
+        System.out.println("Explicit Type Conversion (Narrowing) examples: ==> (" + eDic + "), (" + eBig + "), (" + eInt + ")");
+    }
+}
